c520ce60482489284eeb722675e0dde312ea4945
boldx <boldx@rucsok.hu>
2021-03-21 20:40:02 +0100

    cleanup

--- meow.c
+++ meow.c
@@ -42,7 +42,7 @@ static int find_keyboard_event_file(char *evfname, size_t evfname_len)
 	struct dirent **namelist;
 	int ndev = scandir(DEV_INPUT_EVENT, &namelist, is_event_device, versionsort);
 	if (ndev <= 0) {
-		return -1;
+		return 1;
 	}
 	
 	int highscore = 0;
@@ -76,107 +76,84 @@ static int find_keyboard_event_file(char *evfname, size_t evfname_len)
 			score += 1000;
 		}
 		
-		if (score > highscore && strlen(evfname) < evfname_len) {
+		if (score > highscore && strlen(fname) < evfname_len) {
 			highscore = score;
-			snprintf(evfname, evfname_len, "%s", fname);
+			strcpy(evfname, fname);
 		}
 		
-		release:
-			libevdev_free(dev);
-			close(fd);			
-			free(namelist[i]);
+release:
+		libevdev_free(dev);
+		close(fd);			
+		free(namelist[i]);
 	}
 	
-	return (highscore > 0) ? 0 : -2;
+	return (highscore > 0) ? 0 : 2;
 }
 
 
 static void type_key(const struct libevdev_uinput *uidev,  unsigned int code) {
-    libevdev_uinput_write_event(uidev, EV_KEY, code, 1);
-    libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
-    libevdev_uinput_write_event(uidev, EV_KEY, code, 0);
-    libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
+	libevdev_uinput_write_event(uidev, EV_KEY, code, 1);
+	libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
+	libevdev_uinput_write_event(uidev, EV_KEY, code, 0);
+	libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
 }
 
 
 int main(int argc, char **argv)
 {
-    int err;
-    struct libevdev *dev;
-    struct libevdev_uinput *uidev;
-    
-    char fname[256]; 
-    find_keyboard_event_file(fname, sizeof(fname));
-    printf("%s", fname);
-    //return 0;
-    
-    	int fd = open("/dev/input/event2", O_RDONLY);
-	int rc = libevdev_new_from_fd(fd, &dev);
-	if (rc < 0) {
-		fprintf(stderr, "Failed to init O libevdev (%s)\n", strerror(-rc));
-		exit(1);
+	int rc;
+	char fname[256]; 
+	rc = find_keyboard_event_file(fname, sizeof(fname));
+	if(rc) {
+		fprintf(stderr, "Failed to find a keyboard\n");
+		goto out1;
 	}
-	printf("Input device name: \"%s\"\n", libevdev_get_name(dev));
-	printf("Input device ID: bus %#x vendor %#x product %#x\n",
-	       libevdev_get_id_bustype(dev),
-	       libevdev_get_id_vendor(dev),
-	       libevdev_get_id_product(dev));
+	printf("Source events: %s\n", fname);
 
-    //dev = libevdev_new();
-    //libevdev_set_name(dev, "fake keyboard device");
-/*
-    libevdev_enable_event_type(dev, EV_KEY);
-    libevdev_enable_event_code(dev, EV_KEY, KEY_A, NULL);
-        libevdev_enable_event_code(dev, EV_KEY, KEY_S, NULL);
-*/
-    err = libevdev_uinput_create_from_device(dev,
-        LIBEVDEV_UINPUT_OPEN_MANAGED,
-        &uidev);
-
-    if (err != 0)
-        return err;
-        
-   usleep(100000);
-        
-   if(libevdev_grab(dev, LIBEVDEV_GRAB)) {
-   	printf("grab fail");
-   }
-
-	 int c = 0;
+	int fd = open(fname, O_RDONLY);
+	if (fd < 0) {
+		perror("Failed to open keyboard event device");
+		rc = errno;
+	}
+    
+    	struct libevdev *srcdev;
+	rc = libevdev_new_from_fd(fd, &srcdev);
+	if (rc < 0) {
+		fprintf(stderr, "Failed to init libevdev (%s)\n", strerror(-rc));
+		goto out2;
+	}
+	printf("Source device name: \"%s\"\n", libevdev_get_name(srcdev));
+	
+	struct libevdev_uinput *dstdev;
+	rc = libevdev_uinput_create_from_device(srcdev, LIBEVDEV_UINPUT_OPEN_MANAGED, &dstdev);
+	if (rc) {
+		perror("Failed to create new event device");
+		goto out3;
+	}
+	usleep(100000);
+	
+	rc = libevdev_grab(srcdev, LIBEVDEV_GRAB);
+	if (rc) {
+   		fprintf(stderr, "Failed to grab keyboard event device\n");
+   		goto out3;
+   	}
+	
 	do {
 		struct input_event ev;
-		rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
-		if (rc == 0) {
-			if(0) printf("Event: %s %s %d\n",
-			       libevdev_event_type_get_name(ev.type),
-			       libevdev_event_code_get_name(ev.type, ev.code),
-			       ev.value);
-			libevdev_uinput_write_event(uidev, ev.type, ev.code, ev.value);
-			if (c++ > 30) {
-				c = 0;
-				
-				type_key(uidev, KEY_SPACE);
-				type_key(uidev, KEY_M);
-				type_key(uidev, KEY_E);
-				type_key(uidev, KEY_O);
-				type_key(uidev, KEY_W);
-				type_key(uidev, KEY_SPACE);
-			}
+		rc = libevdev_next_event(srcdev, LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING, &ev);
+		if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
+			libevdev_uinput_write_event(dstdev, ev.type, ev.code, ev.value);
 		}
-		if (rc == -EAGAIN) usleep(10000);
-		
-	} while (rc == 1 || rc == 0 || rc == -EAGAIN);
+	} while (rc == LIBEVDEV_READ_STATUS_SYNC || rc == LIBEVDEV_READ_STATUS_SUCCESS || rc == -EAGAIN);
 	
-    
-
-    libevdev_uinput_write_event(uidev, EV_KEY, KEY_A, 1);
-    libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
-    libevdev_uinput_write_event(uidev, EV_KEY, KEY_A, 0);
-    libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
-    
-    usleep(100000);
-
-
-    libevdev_uinput_destroy(uidev);
-    printf("Complete\n");
+	printf("OK, byee!");
+	rc = 0;
+out3:
+	libevdev_uinput_destroy(dstdev);
+	libevdev_grab(srcdev, LIBEVDEV_UNGRAB);
+out2:
+	libevdev_free(srcdev);
+	close(fd);
+out1:
+	return rc;
 }
